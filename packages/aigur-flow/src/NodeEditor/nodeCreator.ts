571d5c92af52070e989657eb0a70536a37e7943e
Yair Haimo <yair.haimo@gmail.com>
2023-03-06 01:10:58 +0200

    fix types

--- packages/aigur-flow/src/NodeEditor/nodeCreator.ts
+++ packages/aigur-flow/src/NodeEditor/nodeCreator.ts
@@ -1,11 +1,14 @@
 import { makeid } from '@aigur/client/src/makeid';
 
-import { upperFirst } from '../utils/stringUtils';
 import { NodeDefinition } from '../types';
+import { upperFirst } from '../utils/stringUtils';
 
 let nodeCnt = 0;
 
-export function createNode(nodeDefinition: NodeDefinition, position: { x: number; y: number }) {
+export function createNode(
+	nodeDefinition: NodeDefinition,
+	position: { x: number; y: number }
+): any {
 	const newNode = {
 		id: `${nodeDefinition.id}@@${nodeCnt++}`,
 		type: `${nodeDefinition.type}${upperFirst(nodeDefinition.subtype ?? '')}`,
